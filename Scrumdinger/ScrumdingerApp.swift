fe87e72349c0a20ddc66a82badbc216ec827bb4e
mihaly bence <mihaly.bence16@gmail.com>
2023-01-26 21:43:06 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mihaly Bence on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
